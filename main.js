701c2d524c15adfdf99c549870a8fdf36752f44c
Demar Jones <demarjones14@hotmail.com>
2022-01-08 05:23:53 -0500

    add: ability to create grid base on cell size

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const canvas = document.getElementById("canvas");
+const canvasWidth = canvas.clientWidth;
+const canvasHeight = canvas.clientHeight;
+
+let cellSize = 32;
+
+function setCellSize(size) {
+  const cells = document.getElementsByClassName("cell");
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.width = size + "px";
+    cells[i].style.height = size + "px";
+  }
+}
+
+function createGrid(size) {
+  let rowCount = canvasWidth / size;
+  let colCount = canvasHeight / size;
+
+  for (let i = 0; i < colCount; i++) {
+    for (let i = 0; i < rowCount; i++) {
+      canvas.innerHTML += '<div class="cell"></div>';
+    }
+  }
+
+  setCellSize(size);
+}
+createGrid(cellSize);
